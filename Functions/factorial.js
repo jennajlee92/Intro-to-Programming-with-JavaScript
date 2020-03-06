2ba80454e4521492f07ea55148cd77db2aa9aa09
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-06 13:13:06 +0900

    finished loopsAndIterating

--- Functions/factorial.js
+++ Functions/factorial.js
@@ -6,14 +6,20 @@
 120
 */
 
-function times(number1, number2) {
-  let result = number1 * number2;
-  console.log(result);
-  return result;
+let factorial = num => {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
-let oneFactorial = times(1, 1);
-let twoFactorial = times(2, oneFactorial);
-let threeFactorial = times(3, twoFactorial);
-let fourFactorial = times(4, threeFactorial);
-let fiveFactorial = times(5, fourFactorial);
+console.log(factorial(0));     // => 1
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
---
+++ loopsAndIterating/randomNumberBetween.js
@@ -0,0 +1,13 @@
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let tries = 0;
+let result;
+
+do {
+  let result = randomNumberBetween(1, 6);
+  tries += 1;
+} while (result <= 2);
+
+console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
