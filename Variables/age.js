0bf66324777af7b527c081cc990a9affe0aa5009
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-06 12:27:35 +0900

    exercise 1 of loopsAndIterating

--- Variables/age.js
+++ Variables/age.js
@@ -1,7 +1,7 @@
 let readlineSync = require('readline-sync');
 let age = Number(readlineSync.question('How old are you?\n'));
 console.log(`You are ${age} years old.`);
-console.log(`In 10 years, you will be ${age + 10} years old.`);
-console.log(`In 20 years, you will be ${age + 20} years old.`);
-console.log(`In 30 years, you will be ${age + 30} years old.`);
-console.log(`In 40 years, you will be ${age + 40} years old.`);
+
+for (let i = 10; i <= 40; i += 10) {
+  console.log(`In ${i} years, you will be ${age + i} years old.`);
+}
